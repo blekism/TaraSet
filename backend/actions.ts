ae869abb9fdfad3458a99bbd316e7e0e8b9b438c
Dups aure <duplonhannah08@gmail.com>
2026-08-03 22:38:21 +0800

    created  CreateCircle Function

--- backend/actions.ts
+++ backend/actions.ts
@@ -6,7 +6,39 @@ import * as auth from "@/services/auth";
 // import * as cf from "@/repositories/circleFunctions";
 
 export async function CreateCircle(_previousState: any, formdata: FormData) {
+  const supabase = await createClient();
+
+  function generateCircleCode(length = 6): string {
+    const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    return Array.from({ length }, () =>
+      chars[Math.floor(Math.random() * chars.length)]
+    ).join("");
+  }
+
   const circleName = formdata.get("name") as string;
+
+  const { data, error } = await supabase
+    .from("cicles_tbl")
+    .insert({
+      circle_name: circleName,
+      circle_code: generateCircleCode,
+    })
+    .select()
+    .single();
+
+    if (error) {
+      console.log(error);
+      return {
+        code: 0,
+        message: "An error has occurred. Please try again later",
+      };
+    }
+    console.log("data is: ", data);
+    return {
+        code: 0,
+        message: "Circle Created Successfully.",
+        data: data,
+    };
 }
 
 export async function Login(_previousState: any) {
