6bb2e3a444670746e0e08b752596d9d355ddc28a
Dups aure <duplonhannah08@gmail.com>
2026-08-09 22:59:33 +0800

    feat: add function to delete a destination by date ID

--- backend/actions.ts
+++ backend/actions.ts
@@ -243,4 +243,40 @@ export async function DeleteDate(_previousState: any, formdata: FormData) {
     };
 }
 
+export async function DeleteDestination(_previousState: any, formdata: FormData) {
+  const supabase = await createClient();
+
+  // const circleId = formdata.get("circle_id") as string;
+  const date_id = formdata.get("date_id") as string;
+
+  // const result = await ValidateCode(circleId);
+
+  // if (result.code === 0) {
+  //   return {
+  //     code: 0,
+  //     message: "Circle does not exist!",
+  //   }
+  // }
+
+  const { data, error } = await supabase
+    .from("circle_dates_tbl")
+    .delete() 
+    .select()
+    .eq("date_id", date_id);
+
+    if (error) {
+      console.log(error);
+      return {
+        code: 0,
+        message: "An error has occurred. Please try again later",
+      };
+    }
+    console.log("data is: ", data);
+    return {
+        code: 1,
+        message: "Destination Deleted Successfully.",
+        data: data,
+    };
+}
+
 
